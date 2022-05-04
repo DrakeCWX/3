5ed24f9cf44ad3e4cc373bb808496e4791fd1082
DrakeCWX <103395236+DrakeCWX@users.noreply.github.com>
2022-05-04 09:56:07 +0800

    Add files via upload

---
+++ aa.js
@@ -0,0 +1,6 @@
+text=prompt("Please enter your identity(student or parents)")
+    
+if (text=="student"){  location.replace("https://drakecwx.github.io/3/course.html")
+}
+if (text=="parents"){  location.replace("https://drakecwx.github.io/3/index.html")
+}
